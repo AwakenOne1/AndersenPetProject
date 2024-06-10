4fb2b5541049875c861e57642cb27cf9489bb0e8
AwakenOne1 <alekseydubovik2003@icloud.com>
2024-06-10 10:25:10 +0300

    [added] Structure for working with API

---
+++ AndersenPetProject/ImageInfo.swift
@@ -0,0 +1,55 @@
+//
+//  ImageInfo.swift
+//  AndersenPetProject
+//
+//  Created by Alexey Dubovik on 10.06.24.
+//
+
+import Foundation
+
+import Foundation
+import CoreData
+
+struct ImageInfo: Decodable {
+    let id: String
+    let slug: String
+    let altDescription: String?
+    let urls: Urls
+    var isFavourite = false
+
+    enum CodingKeys: String, CodingKey {
+        case id
+        case slug
+        case altDescription = "alt_description"
+        case urls
+    }
+
+    init(id: String, slug: String, altDescription: String?, urls: Urls, isFavourite: Bool = false) {
+        self.id = id
+        self.slug = slug
+        self.altDescription = altDescription
+        self.urls = urls
+        self.isFavourite = isFavourite
+    }
+
+    init(from decoder: any Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.id = try container.decode(String.self, forKey: .id)
+        self.slug = try container.decode(String.self, forKey: .slug)
+        self.altDescription = try container.decodeIfPresent(String.self, forKey: .altDescription)
+        self.urls = try container.decode(Urls.self, forKey: .urls)
+    }
+
+}
+
+struct Urls: Codable {
+    let small: String
+    let regular: String
+    var smallURL: URL {
+        return URL(string: small)!
+    }
+    var regularURL: URL {
+        return URL(string: regular)!
+    }
+
+}
